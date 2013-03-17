3d9dbb50d82c929371280719b036274d570d2f6d
Ivan Puchkov <rake.eclipse@gmail.com>
2013-03-17 21:42:48 +0700

    Add Active Admin

---
+++ app/admin/comments.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register Comment, :as => "UserComments" do
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Blog::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   get "home/index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   root :to => 'home#index'
 end
